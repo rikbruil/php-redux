ccee9f94072187943b9a843bf06485550f3c0a0b
Rik Bruil <rikbruil@users.noreply.github.com>
2016-01-17 22:01:30 +0100

    License

--- src/Store.php
+++ src/Store.php
@@ -1,5 +1,13 @@
 <?php
 
+/*
+ * This file is part of the Rephlux library.
+ * (c) Rik Bruil <rikbruil@users.noreply.github.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Rb\Rephlux;
 
 use Rb\Rephlux\Exception\InvalidFlowException;
